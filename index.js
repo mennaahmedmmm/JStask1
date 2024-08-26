addbdededce01f97ba8d0cd923db414619c80a28
mennaahmed <mennaahmed@357892005@gmail.com>
2024-08-26 12:15:42 +0300

    score between 85 and 75

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let score=window.prompt("What is your score");
-if(score <100 && score > 85){
+if(score < 100 && score >= 85){
     document.write("You got a A 🎉.");
     console.log("You got a A 🎉.");
+}
+else if(score < 85 && score >= 75){
+    document.write("You got a B 🎉.");
+    console.log("You got a B 🎉.");
 }
